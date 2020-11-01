ec672724f9067280aa4f0ba8dc7f9f8a7b61dcd2
RomainMouly <romainmouly@gmail.com>
2020-11-01 08:34:04 +0100

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import './Contact.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { online: props.online };
+  }
 
+  Status = () => {
+    const newStatus = !this.state.online;
+    this.setState({ online: newStatus });
+  };
 
+  render() {
+    const { avatar } = this.props;
+    const { name } = this.props;
+    const { online } = this.state;
 
-class Contact extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={online : props.online}
-    }
-
-
-    Status =() => {
-        const newStatus = !this.state.online;
-        this.setState({ online : newStatus });
-    }
-
-    render(){
-
-        const{avatar}=this.props;
-        const{name}=this.props;
-        const{online}=this.state;
-
-    
-        return(
-
-            <div className='Contact'>
-            <img className='avatar' src={avatar} alt={name}/>
-            <div>
-                <div className='name'>{name}</div>
-                <div className='status'>
-                    <div className={online?'status-online' : 'status-offline'} onClick={()=>this.Status()}></div>
-                    <div className='status-text' onClick={()=>this.Status()}>{online ? 'online' : 'offline'}</div>
-                </div>
+    return (
+      <div className="Contact">
+        <img className="avatar" src={avatar} alt={name} />
+        <div>
+          <div className="name">{name}</div>
+          <div className="status">
+            <div
+              className={online ? "status-online" : "status-offline"}
+              onClick={() => this.Status()}
+            ></div>
+            <div className="status-text" onClick={() => this.Status()}>
+              {online ? "online Salut" : "offline"}
             </div>
-            </div>
-
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-    Contact.propTypes = {
-
-        name : PropTypes.string,
-        avatar : PropTypes.string,
-        online : PropTypes.bool,
-        
-        
-
-    }
+Contact.propTypes = {
+  name: PropTypes.string,
+  avatar: PropTypes.string,
+  online: PropTypes.bool,
+};
 
 export default Contact;
